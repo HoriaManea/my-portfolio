ffe358b918f89a888cf78cae23244989664b9c81
Horia Manea <hmanea@c4sam.com>
2025-08-05 15:51:35 +0300

    delete(): delete MyAccount button from BookCard Component

--- src/components/Courses/BookCard.js
+++ src/components/Courses/BookCard.js
@@ -13,10 +13,7 @@ function BookCard(props) {
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "" : "MyAccount"}
-        </Button>
+        
         {"\n"}
         {"\n"}
 
